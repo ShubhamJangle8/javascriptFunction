33fe56fceddfb6c47e02d2b9b960a24b33002e09
jangle <shubhamjangale0000@gmail.com>
2020-11-13 07:19:55 +0530

    Added : Added functionality for checking whether two numbers are palindrome

---
+++ CheckForPalindromes.js
@@ -0,0 +1,18 @@
+let num1 = process.argv[2];
+let num2 = process.argv[3];
+let temp = 0, rem = 0, sum = 0;
+function isPalindrome(num1, num2) {
+    while (num1 > 0) {
+        temp = num1;
+        rem = temp % 10;
+        num1 = Math.floor(num1 / 10);
+        sum = (sum * 10) + rem;
+    }
+    if (num2 == sum) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+console.log(isPalindrome(num1, num2));
